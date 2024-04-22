5eaed3841c8f30875f207802f23e591312bee24b
D9292S <kdeepankar40@gmail.com>
2024-04-22 08:39:42 +0530

    banking, main file modified with errors

--- Banking/banking.py
+++ Banking/banking.py
@@ -1,7 +1,9 @@
 import json
-
+import hashlib
 class BankAccount:
-    def __init__(self, client_id, first_name, last_name, account_no, crn_number, password, branch):
+    VALID_ACCOUNT_TYPES = ["Savings Account", "Current Account"]
+
+    def __init__(self, client_id, first_name, last_name, account_no, crn_number, password, branch, Savings_Account = 0, Current_Account= 0):
         self.client_id = client_id
         self.first_name = first_name
         self.last_name = last_name
@@ -9,35 +11,79 @@ class BankAccount:
         self.crn_number = crn_number
         self.password = password
         self.branch = branch
+        self.Savings_Account = Savings_Account
+        self.Current_Account = Current_Account
+        self.clients = self._load_bank_data()
 
-    def balance(self):
-        self.open_account()
-        clients = self.get_bank_data()
-        savings = clients[str(self.client_id)]["Savings Account"]
-        current = clients[str(self.client_id)]["Current Account"]
-        return savings, current
+    @staticmethod
+    def _hash_password(password):
+        return hashlib.sha256(password.encode()).hexdigest()
+
+    @property
+    def balance(self, client_id):
+        client = self.clients.get(str(client_id), {})
+        return client.get("Savings Account", 0) , client.get("Current Account", 0)
+    
+    def _get_client(self):
+        return self.clients.get(str(self.client_id), {})
     
     def open_account(self):
-        clients = self.get_bank_data()
-        if str(self.client_id) not in clients:
-            clients[str(self.client_id)] = {
-                "First Name": self.first_name,
-                "Last Name": self.last_name,
-                "Account Number": self.account_no,
-                "CRN Number": self.crn_number,
-                "Password": self.password,
-                "Branch": self.branch,
-                "Savings Account": 0,
-                "Current Account": 0
-            }
-        
+        if str(self.client_id) in self.clients:
+            raise ValueError("An account with this client_id already exists.")
+        self.clients[str(self.client_id)] = {
+            "First Name": self.first_name,
+            "Last Name": self.last_name,
+            "Account Number": self.account_no,
+            "CRN Number": self.crn_number,
+            "Password": self.password,
+            "Branch": self.branch,
+            "Savings Account": self.Savings_Account,
+            "Current Account": self.Current_Account
+        }
+        self._save_bank_data()
+
+    def deposit(self, client_id, amount, account_type):
+        if account_type not in self.VALID_ACCOUNT_TYPES:
+            raise ValueError(f"Invalid account type. Expected one of {self.VALID_ACCOUNT_TYPES}")
+        if amount <= 0:
+            raise ValueError("Deposit amount must be positive.")
+        if account_type == "Savings Account":
+            self.clients[str(client_id)]["Savings Account"] += amount
+        else:
+            self.clients[str(client_id)]["Current Account"] += amount
+        self._save_bank_data()
+
+    def _load_bank_data(self):
+        try:
+            with open("mainbank.json", "r") as f:
+                return json.load(f)
+        except FileNotFoundError:
+            return {}
+
+    def _save_bank_data(self):
         with open("mainbank.json", "w") as f:
-            json.dump(clients, f, indent=4)
+            json.dump(self.clients, f, indent=4)
 
-        return True 
-    
-    def get_bank_data(self):
-        with open('mainbank.json', 'r') as f:
-            clients = json.load(f)
+class Transaction:
+    def __init__(self, bank_account):
+        self.bank_account = bank_account
 
-        return clients
+    def deposit(self, amount, account_type):
+        self.bank_account.deposit(amount, account_type)
+
+    def withdraw(self, amount, account_type):
+        if self.bank_account.clients[str(self.bank_account.client_id)][account_type] < amount:
+            raise ValueError("Insufficient balance.")
+        self.bank_account.clients[str(self.bank_account.client_id)][account_type] -= amount
+        self.bank_account._save_bank_data()
+
+class BankManager:
+    def __init__(self, bank_account):
+        self.bank_account = bank_account
+
+    def open_account(self):
+        self.bank_account.open_account()
+
+    def close_account(self):
+        del self.bank_account.clients[str(self.bank_account.client_id)]
+        self.bank_account._save_bank_data()
--- Banking/main.py
+++ Banking/main.py
@@ -4,100 +4,187 @@ import time
 from tqdm import tqdm
 import os
 
+from getpass import getpass 
+
 os.chdir("D:\\75 Days Python Mastery\\Banking")
-print("*** Welcome to DS Bank ***")
-print()
-print("NOTE: Dear user, we do not ask for your personal information such as ATM PIN, Card No, CVV, etc.")
-print("Kindly do not share it with anyone")
-print()
-print("To Continue, press 1. To exit, press any other key.")
-print()
-ch = input("Enter your choice: ")
-if ch == '1':
-    print("**** Welcome To DS Netbanking Portal, Kindly Enter your Unique Customer Id *****")
+
+
+def welcome_screen():
     print()
-    client_id = input("Enter your Unique Customer Id, If you don't have one, press any key to Register: ")
+    print("*** Welcome to DS Bank ***")
+    print()
+    print("NOTE: Dear user, we do not ask for your personal information such as ATM PIN, Card No, CVV, etc.")
+    print("Kindly do not share it with anyone")
+    print()
+    print("To Continue, press 1, To exit press any other key.")
+
+def netbanking_portal():
+    print()
+    print("--------------------------------------------------------------------------------------")
+    print("| ****** Welcome To DS Netbanking Portal, Kindly Enter your Unique Customer Id ***** |")
+    print("--------------------------------------------------------------------------------------")
     print()
 
-    bank_account = BankAccount(client_id, "", "", "", "", "", "")
-    if client_id in bank_account.get_bank_data():
-        password = input("Enter your Password: ")
-        bank_account = BankAccount(client_id, "", "", "", "", password, "")
-        if password != bank_account.get_bank_data()[str(client_id)]["Password"]:
-            print("Invalid Password, Please Try Again")
-            exit()
+
+def banking_services_menu():
+    print("+-----------------------------------------+")
+    print("|             DS Bank Services            |")
+    print("+-----------------------------------------+")
+    print("| 1. Banking Services                     |")
+    print("| 2. Card Services                        |")
+    print("| 3. Loan Services                        |")
+    print("| 4. Customer Services                    |")
+    print("| Any Other Key to Exit                   |")
+    print("+-----------------------------------------+")
+    print()
+
+def banking_services(bank_account, client_id):
+    choice = input("Enter your choice: ")
+    print()
+    while True:
+        if choice == '1':
+            print("+----------------------------------------+")
+            print("|          Banking Services Menu         |")
+            print("+----------------------------------------+")
+            print("| 1. Check Balance                       |")
+            print("| 2. Deposit Money                       |")
+            print("| 3. Withdraw Money                      |")
+            print("| 4. Transfer Money                      |")
+            print("| Pres Any Other Key to Exit             |")
+            print("+----------------------------------------+")
+            print()
+
+            choice = input("Enter your choice: ")
+            print()
+
+            if choice == '1':
+                savings, current = bank_account.balance(client_id)
+                print("+--------------------------------------------+")
+                print(f"| Your Savings Account Balance is: {savings} |")
+                print(f"| Your Current Account Balance is: {current} |")
+                print("+--------------------------------------------+")
+
+            elif choice == '2':
+                print("Please Choose the type of Account you want to deposit money into..")
+                print()
+                print("Press 1 for Savings Account")
+                print("Press 2 for Current Account")
+                print()
+                choice = input("Enter your choice: ")
+                print()
+                if choice == '1':
+                    amount = int(input("Enter the amount to be deposited: "))
+                    bank_account.deposit(client_id, amount, "Savings Account")
+                    print(f"Amount of ${amount} deposited successfully in Savings Account")
+                else:
+                    amount = int(input("Enter the amount to be deposited: "))
+                    bank_account.deposit(client_id, amount, "Current Account")
+                    print(f"Amount of ${amount} deposited successfully in Current Account")
+
+            elif choice == '3':
+                amount = int(input("Enter the amount to be withdrawn: "))
+                account_type = input("Enter the account type (Savings Account/Current Account): ")
+                bank_account.withdraw(amount, account_type)
+                print("Amount Withdrawn Successfully")
+
+            elif choice == '4':
+                amount = int(input("Enter the amount to be transferred: "))
+                account_type = input("Enter the account type (Savings Account/Current Account): ")
+                bank_account.transfer(amount, account_type)
+                print("Amount Transferred Successfully")
+
+            else:
+                print("Thank You for using DS Bank")
+                break
+
+
+def login(client_id, bank_account):
+    clients = bank_account._load_bank_data()
+    for i in range(3, 0, -1):
+        password = getpass("Enter your Password: ")
+        if password != clients[str(client_id)]["Password"]:
+            print(f"Invalid Password, Please Try Again: Attempts Left {i-1}")
+            if i == 1:
+                print("You have entered the wrong password multiple times, Kindly try again after 24 hours later.")
+                exit(0)
+        else:
+            break
+        
+
+def register():
+    print("You are not a registered user, Kindly Register Yourself")
+    print()
+    first_name = input("Enter your First Name: ")
+    last_name = input("Enter your Last Name: ")
+    account_number = random.randint(10000000000000, 999999999909999)
+    crn_number = random.randint(100000, 999999)
+    client_id = first_name[:4] + str(random.randint(100000, 999999))
+    password = first_name[:2].upper() + last_name[:2].lower() + '@' + str(random.randint(100000, 999999))
+    branch = "Codex Discord"
+    bank_account = BankAccount(client_id, first_name, last_name, account_number, crn_number, password, branch)
+    bank_account.open_account()
+    print()
+    print("Please Wait while we open an account for you...")
+    print()
+    time.sleep(5)
+    print("We are connecting to our server, Kindly wait for a moment....")
+    print('Connecting', end='')
+
+    for i in tqdm(range(100)):
+        time.sleep(0.1)
+
+    print()
+    print()
+    time.sleep(3)
+    print("Congratulations, We have successfully opened an account for you!")
+    print("Please Note Down your Account Details for Future Reference..........")
+    print()
+    print("****************************************************")
+    print("*                                                  *")
+    print("*                Account Details                   *")
+    print("*                                                  *")
+    print("****************************************************")
+    print("* Client Id: ", client_id, " "*(35-len(client_id)), "*")
+    print("* First Name: ", first_name, " "*(34-len(first_name)), "*")
+    print("* Last Name: ", last_name, " "*(35-len(last_name)), "*")
+    print("* Account Number: ", account_number, " "*(30-len(str(account_number))), "*")
+    print("* CRN Number: ", crn_number, " "*(34-len(str(crn_number))), "*")
+    print("* Password: ", password, " "*(36-len(password)), "*")
+    print("* Branch: ", branch, " "*(38-len(branch)), "*")
+    print("*                                                  *")
+    print("****************************************************")
+    print()
+    print("Thank You for choosing DS Bank")
+    print("Thank You for using DS Netbanking Portal, Have a Nice Day!")
+
+
+
+def main():
+    welcome_screen()
+    ch = input("Enter your choice: ")
+    if ch == '1':
+        netbanking_portal()
+        client_id = input("Enter your Unique Customer Id, If you don't have one, press any key to Register: ")
+        print()
+        bank_account = BankAccount(client_id, "", "", "", "", "", "", "", "")
+        if client_id in bank_account._load_bank_data():
+            login(client_id, bank_account)
+            print()
+            print(f"** Login Successful, Welcome user {client_id} **")
+            print()
+            banking_services_menu()
+            banking_services(bank_account, client_id)
 
         else:
-            print()
-            print(f"Welcome Back **{client_id}**!")
-            print("Press 1. Check Balance")
-            print("Press 2. Deposit Money")
-            print("Press 3. Withdraw Money")
-            print("Press 4. Transfer Money")
-            print("Press Any Other Key to Exit")
-            print()
-
-            choice = int(input("Enter your choice: "))
-            print()
-
-            if choice == 1:
-                print("Your Account Balance is: ", bank_account.balance())
-            elif choice == 2:
-                amount = int(input("Enter the amount to be deposited: "))
-                bank_account.deposit(amount)
-                print("Amount Deposited Successfully")
-            elif choice == 3:
-                amount = int(input("Enter the amount to be withdrawn: "))
-                bank_account.withdraw(amount)
-                print("Amount Withdrawn Successfully")
-            elif choice == 4:
-                recipient_id = input("Enter the recipient's Customer Id: ")
-                amount = int(input("Enter the amount to be transferred: "))
-                bank_account.transfer(recipient_id, amount)
-                print("Amount Transferred Successfully")
-            else:
-                print("Thank You for using DS Netbanking Portal")
-    
-
-
-
+            register()
     else:
-        print("You are not a registered user, Kindly Register Yourself")
-        first_name = input("Enter your First Name: ")
-        last_name = input("Enter your Last Name: ")
-        account_number = random.randint(10000000000000, 999999999909999)
-        crn_number = random.randint(100000, 999999)
-        client_id = first_name[:4] + str(random.randint(100000, 999999))
-        password = first_name[:2].upper() + last_name[:2].lower() + '@' + str(random.randint(100000, 999999))
-        branch = "Codex Discord"
-        bank_account = BankAccount(client_id, first_name, last_name, account_number, crn_number, password, branch)
-        bank_account.open_account()
-        print("Please Wait while we open an account for you...")
-        print()
-        time.sleep(5)
-        print("We are connecting to our server, Kindly wait for a moment....")
-        print('Connecting', end='')
+        print("Thank You for using DS Bank")
 
-        for i in tqdm(range(100)):
-            time.sleep(0.1)
+if __name__ == "__main__":
+    main()
 
-        print()
-        print()
-        time.sleep(3)
-        print("Congratulations, We have successfully opened an account for you")
-        print("Please Note Down your Account Details for Future Reference")
-        print()
-        print("Client Id: ", client_id)
-        print("First Name: ", first_name)
-        print("Last Name: ", last_name)
-        print("Account Number: ", account_number)
-        print("CRN Number: ", crn_number)
-        print("Password: ", password)
-        print("Branch: ", branch)
-        print()
-        print("Thank You for choosing DS Bank")
-        print("Thank You for using DS Netbanking Portal")
-else:
-    print("Thank You for using DS Netbanking Portal")
+
+
+
 
 
